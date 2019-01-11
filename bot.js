dab6453fce99de19dfa5bc51b6fddaec0d6af7bc
rwnwn <45011018+rwnwn@users.noreply.github.com>
2019-01-11 04:21:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -352,7 +352,7 @@ client.on('message', message => {
 
 
 
-const devs = ["394949560327208960"]; //الايدي حقك
+const devs = ["489322902886547457"]; //الايدي حقك
 const adminprefix = ["F"]; // البرفكس
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
